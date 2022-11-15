6260bde719c5a2f7d344e02436283d2901fcabe5
notorious <huynhtrieutrivi@gmail.com>
2022-11-15 02:42:43 -0800

    Massive restructuring and implemented EJS

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const express = require('express')
 var allstudent = require('./students.json')
 const app = express()
 
-app.use('/css',express.static(__dirname + '/src/public/css'));
+// Static Rendering
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/images', express.static(__dirname + '/public/images'));
+
+// Encoding
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+// View Engine
 app.set('view engine', 'ejs')
 
-
+// Express Routing
 app.get('/', (req, res) => {
   res.render("index.ejs")
 })
 
 app.get('/login', (req, res) => {
-  res.sendFile("views/login.ejs")
+  res.render("login.ejs")
 })
 
 

8ff6634653f1b1a448d3dd7d72dd2b6a0f1532c1
jesus1a <48657218+jesus1a@users.noreply.github.com>
2022-11-14 19:55:55 -0800

    Update controller.js
    added a login credential return function

--- src/models/controller.js
+++ src/models/controller.js
@@ -84,5 +84,27 @@ async function returnTable(gtable){
     });
     return await p;
 }
+//function that can return the id, username, and password of a user from a desired table (Student or Faculty)
+async function returnLogin(gtable, id){
+    var mysql = require('mysql');
+    
+    var con = mysql.createConnection({
+        host: "fcfs.c2oe7fkglsr2.us-west-2.rds.amazonaws.com",
+        user: "admin",
+        password: "529dh-bj345-wbedaj",
+        database: "FCFS"
+    });
 
-module.exports = {Reg, returnName, returnTable}
+    let p = new Promise(function(res,rej) {
+        con.connect(function(err){
+            if (err) throw err;
+            con.query(`SELECT id, userName, password FROM ${gtable} WHERE id = ${id}`, function(err, result, fields) {
+                if (err) throw err;
+                res(result);
+            });
+        });    
+    });
+    return await p;
+}
+
+module.exports = {Reg, returnName, returnTable, returnLogin}

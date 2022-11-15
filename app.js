678a14749ff22289817f67972af215f2c51fe173
notorious <huynhtrieutrivi@gmail.com>
2022-11-15 02:26:34 -0800

    Configure view engine to ejs, routing css files, rendering css, images, for dynamic rendering

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 const { json } = require('express')
 const express = require('express')
+var allstudent = require('./students.json')
 const app = express()
 
-app.use(express.static('src/public'))
+app.use('/css',express.static(__dirname + '/src/public/css'));
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.set('view engine', 'ejs')
 
-const users = [{name: "Name"}]
-
-app.get('/users', (req, res) => {
-  res.json(users)
-})
-
-app.post('/users', (req, res) => {
-  const user = {name: req.body.name, password: req.body.password};
-  users.push(user)
-  res.status(201).send()
-})
 
 app.get('/', (req, res) => {
-  res.sendFile("src/public/index.html")
+  res.render("index.ejs")
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile("views/login.ejs")
 })
 
 

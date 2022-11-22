ecb5ae64a9e3d9207478a19567483cc4427981fd
jesus1a <48657218+jesus1a@users.noreply.github.com>
2022-11-21 17:28:13 -0800

    Update adminClass.js
    fixed some misspellings

--- models/adminClass.js
+++ models/adminClass.js
@@ -10,8 +10,8 @@ class Admin {
 
 
     }
-
-    facultyReg(gname, gusername, fpassword, gtele, gofficeTele, gofficeHours, gofficeNum, gtitle) {
+//faculty registration creates and adds a faculty memeber into the faculty table
+    facultyReg(gname, gusername, gpassword, gtele, gofficeTele, gofficeHours, gofficeNum, gtitle) {
 
         var mysql = require('mysql');
 
@@ -30,7 +30,7 @@ class Admin {
 
             console.log("connected");
             var sql = `INSERT INTO Faculty(name, userName, password, tele, officeTele, officeHours, officeNum, title)
-            VALUES("${gname}", "${gpassword}", "${gtele}", "${gofficeTele}", "${gofficeHours}", "${gofficeNum}", "${title}")`;
+            VALUES("${gname}",  "${gusername}", "${gpassword}", "${gtele}", "${gofficeTele}", "${gofficeHours}", "${gofficeNum}", "${gtitle}")`;
             con.query(sql, function
                 (err, result) {
                 if (err) throw err;
@@ -39,31 +39,31 @@ class Admin {
 
         });
 }
+//Student registration creates and adds a student into the student registration
+    studentReg(sId, sName, sAddress, sUserName, sPassword, sClassSchedule, sTele, sMajor, sMinor, sNotes) {
 
-studentReg(sId, sName, sAddress, sUserName, sPassword, sClassSchedule, sTele, sMajor, sMinor, sNotes) {
+        var mysql = require('mysql');
 
-    var mysql = require('mysql');
+        var con =
+            mysql.createConnection({
+                host:
+                    "fcfs.c2oe7fkglsr2.us-west-2.rds.amazonaws.com",
+                user: "admin",
+                password: "529dh-bj345-wbedaj",
+                database: "FCFS"
+            })
 
-    var con =
-        mysql.createConnection({
-            host:
-                "fcfs.c2oe7fkglsr2.us-west-2.rds.amazonaws.com",
-            user: "admin",
-            password: "529dh-bj345-wbedaj",
-            database: "FCFS"
-        })
+        con.connect(function (err) {
+            if (err) throw err;
 
-    con.connect(function (err) {
-        if (err) throw err;
-
-        console.log("connected");
-        var sql = `INSERT INTO Student(id, name, address, userName, password, classSchedule, telephone,major, minor, notes) VALUES("${sId}", "${sName}", "${sAddress}", "${sUserName}", "${sPassword}", "${sClassSchedule}", "${sTelephone}" "${sMajor}", "${sMinor}", "${sNotes}")`;
-        con.query(sql, function
-            (err, result) {
-            if(err) throw err;
-            console.log("1 record inserted");
+            console.log("connected");
+            var sql = `INSERT INTO Student(id, name, address, userName, password, classSchedule, telephone,major, minor, notes) VALUES("${sId}", "${sName}", "${sAddress}", "${sUserName}", "${sPassword}", "${sClassSchedule}", "${sTele}" "${sMajor}", "${sMinor}", "${sNotes}")`;
+            con.query(sql, function
+                (err, result) {
+                if(err) throw err;
+                console.log("1 record inserted");
+                });
             });
-        });
 
-}//StudentReg
+    }
 }

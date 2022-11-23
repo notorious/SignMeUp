d29312ccbdb84770c568a58d1736254e6d80b312
notorious <huynhtrieutrivi@gmail.com>
2022-11-23 04:39:31 -0800

    express-favicon
    Set favicon for every route via express-favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('express-favicon');
 var session = require('express-session');
 var indexRouter = require('./routes/index');
 
@@ -23,7 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
+// Routes
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler

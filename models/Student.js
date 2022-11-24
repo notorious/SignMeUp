117fd6840c5431f6ef0ba5fab5276a106beb06cb
jesus1a <48657218+jesus1a@users.noreply.github.com>
2022-11-24 02:05:42 -0800

    Update Student.js
    changed how class schedule works now that strings have become viable

--- models/Student.js
+++ models/Student.js
@@ -1,11 +1,13 @@
 class Student{
-    constructor (id, name, address, userName, password, classSchedule, tele, major, minor, notes) {
+    constructor (id, name, address, userName, password, classComplete, classCurrent, classExt, tele, major, minor, notes) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.userName = userName;
         this.password = password;
-        this.classSchedule = classSchedule;
+        this.classComplete = classComplete;
+        this.classCurrent = classCurrent;
+        this.classExt = classExt;
         this.tele = tele;
         this.major = major;
         this.minor = minor;
@@ -66,6 +68,10 @@ class Student{
         this.studentUpdate("notes", newNotes, id);
     }
 
+    returnClassSchedule(){
+
+    }
+
     studentUpdate(gColumn, gUpdate, id){
         var mysql = require('mysql');
     
@@ -89,18 +95,17 @@ class Student{
     }
 }
 
-
 async function studentlogOn(userName, password){
     let p = new Promise(function(res,rej) {
     (async () => {
         temp = await returnLogin("Students", userName, password);
         console.log(temp);
         if (temp[0] == null){
-            res(null);
+            res([null, -1]);
         }
         else{
-            test = new Student(temp[0].id, temp[0].name, temp[0].address , userName, password, temp[0].classSchedule, temp[0].tele, temp[0].major, temp[0].minor, temp[0].notes);
-            res(test);
+            test = new Student(temp[0].id, temp[0].name, temp[0].address , userName, password, temp[0].classComplete, temp[0].classCurrent, temp[0].classExt, temp[0].tele, temp[0].major, temp[0].minor, temp[0].notes);
+            res([test, 0]);
         }
         })();
     });
@@ -128,18 +133,3 @@ async function returnLogin(gtable, userName, password){
     });
     return await p;
 }
-/*
-(async () => {
-    //temp = await dt.returnTable("Students");
-    temp = await studentlogOn("testUpdate", "1234");
-    //console.log(temp);
-    test = temp[0];
-    code = temp[1];
-    console.log(test)
-    if (test == null) {
-        console.log("User does not exist, check credentials before running again")
-        return;
-    }
-    test.userNameUpdate("TestUpdate");
-})();
-*/

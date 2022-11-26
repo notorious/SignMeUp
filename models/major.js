d44d8cdf4bde72de4706bb072e67b3c7a3753d1a
jesus1a <48657218+jesus1a@users.noreply.github.com>
2022-11-26 14:58:47 -0800

    Update major.js
    in progress major outline function

--- models/major.js
+++ models/major.js
@@ -1,41 +1,50 @@
-class Major {
-    constructor(studentMajor, studentClass) {
-        this.studentMajor = studentMajor;
-        this.studentClass = studentClass;
+async function returnMajor(gmajor){
+    var mysql = require('mysql');
+    
+    var con = mysql.createConnection({
+        host: "fcfs.c2oe7fkglsr2.us-west-2.rds.amazonaws.com",
+        user: "admin",
+        password: "529dh-bj345-wbedaj",
+        database: "FCFS"
+    });
 
-    }
-
-
-    async returnCourses(studentMajor, studentClass) {
-        var mysql = require('mysql');
-        
-        var con = mysql.createConnection({
-            host: "fcfs.c2oe7fkglsr2.us-west-2.rds.amazonaws.com",
-            user: "admin",
-            password: "529dh-bj345-wbedaj",
-            database: "FCFS"
-        });
-
-        let Major = this.studentMajor;
-
-
-
-        let p = new Promise(function(res,rej) {
-            con.connect(function(err){
+    let p = new Promise(function(res,rej) {
+        con.connect(function(err){
+            if (err) throw err;
+            con.query(`SELECT courseReq FROM Major WHERE majorTitle = "${gmajor}"`, function(err, result, fields) {
                 if (err) throw err;
-                con.query("SELECT * FROM Major WHERE Courses = studentClass", function (err, result) {
-                    if (err) throw err;
-                    res(result);
-                    if (result.length == 0)
-                        console.log("Course not valid")
-                    else
-                        console.log("Course accepted")
-                    
-                });
-            });    
-        });
-        return await p;
-    }
+                res(result);
+            });
+        });    
+    });
+    return await p;
 }
 
-module.exports = {returnCourses};
+async function returnMajorOutline(gid){
+    var mysql = require('mysql');
+    
+    var con = mysql.createConnection({
+        host: "fcfs.c2oe7fkglsr2.us-west-2.rds.amazonaws.com",
+        user: "admin",
+        password: "529dh-bj345-wbedaj",
+        database: "FCFS"
+    });
+
+    let p = new Promise(function(res,rej) {
+        con.connect(function(err){
+            if (err) throw err;
+            con.query(`SELECT selectedCourses, statusHistory FROM MajorOutline WHERE studentID = ${gid}`, function(err, result, fields) {
+                if (err) throw err;
+                if (result[0] != null){
+                    var temp = result[0].selectedCourses;
+                    var temp2 = result[0].statusHistory;
+                    var array1;
+                }
+                res(result);
+            });
+        });    
+    });
+    return await p;
+}
+
+module.exports = {returnMajor, returnMajorOutline};
